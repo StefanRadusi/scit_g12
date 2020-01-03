8d5d809e6c3746ef7b55d24ab8360354fe0768d2
Delia Popescu <deliabuze@gmail.com>
2020-01-04 01:05:54 +0200

    Solving Homework 1 and 2

--- script.js
+++ script.js
@@ -2,11 +2,42 @@ console.log("start");
 
 // example of reacting to button click
 const sendButton = document.getElementById("send");
+const addButton = document.getElementById("addFiled");
+
 
 // this the function that will be called when click event is triggered
 function onClickSendButton() {
-  console.log("click on", sendButton);
+    console.log("click on", sendButton);
 }
+
+document.getElementById("fieldName").oninput = function() {
+  this.style.borderColor = null;
+};
+
+function addNewField() {
+    const inputField = document.getElementById("fieldName");
+    const fieldName = inputField.value;
+    if (!fieldName) {
+        inputField.style.borderColor = "red";
+    } else {
+        const newDiv = document.createElement("div");
+        newDiv.setAttribute("class", "field");
+        const form = document.getElementsByClassName("form-fields")[0];
+        form.appendChild(newDiv);
+
+        const newP = document.createElement("p");
+        newP.innerText = fieldName;
+        newDiv.appendChild(newP);
+
+        const newInput = document.createElement("input");
+        newInput.id = fieldName;
+        newDiv.appendChild(newInput);
+    }
+}
+
 
 // we attached a function to click event triggered by send button
 sendButton.addEventListener("click", onClickSendButton);
+addButton.addEventListener("click", addNewField);
+
+
