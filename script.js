1474e21eaf210e6ddc250c1839998815f7fed3bb
unknown <cebotari.luminita1@gmail.com>
2020-01-05 21:01:01 +0200

    my homework

--- script.js
+++ script.js
@@ -1,11 +1,29 @@
 console.log("start");
 
+function addContainer () {
+  const divContainer = document.createElement("div");
+  divContainer.className = "field"; 
+  const newH1 = document.createElement("h1");
+  const newP = document.createElement("p");
+  const addImput = document.createElement("input"); 
+
+  const imputTextField = document.getElementById("fieldName").value;
+  addImput.id = imputTextField; 
+  newP.innerHTML = imputTextField; 
+  divContainer.appendChild(newH1);
+  divContainer.appendChild(newP);
+  divContainer.appendChild(addImput);
+  
+
+  document.getElementsByClassName("form-fields")[0].appendChild(divContainer);
+}
 // example of reacting to button click
-const sendButton = document.getElementById("send");
+const sendButton = document.getElementById("addFiled");
 
 // this the function that will be called when click event is triggered
 function onClickSendButton() {
-  console.log("click on", sendButton);
+  console.log("click on", addContainer);
+  addContainer();
 }
 
 // we attached a function to click event triggered by send button
