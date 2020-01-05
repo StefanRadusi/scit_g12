148ab3a89e131b5ee9b162621b164e737e336ece
Flaviusmadarassi <flavius.madarassi@yahoo.com>
2020-01-06 00:03:45 +0200

    I did not found the final solution

--- script.js
+++ script.js
@@ -10,3 +10,54 @@ function onClickSendButton() {
 
 // we attached a function to click event triggered by send button
 sendButton.addEventListener("click", onClickSendButton);
+
+
+//  MY SOLUTION
+
+let userInputs = document.querySelectorAll(".form-fields input");
+
+const AddFieldBtn = document.getElementById("addField");
+
+
+sendButton.addEventListener("click", function(){
+  userInputs.forEach(function(entry){
+    if (entry.value != '' || entry.value != entry.defaultValue){
+      entry.style.borderColor = "lightgrey";
+    }else 
+      entry.style.borderColor = "red";
+  })
+})
+
+const userInput = document.getElementById("fieldName");
+
+function createNewDiv(){
+
+  const divContainer = document.createElement("div");
+  divContainer.classList.add("field");
+
+  const fieldN = document.createElement("p");
+  const createInput = document.createElement("input");
+  
+  if (userInput.value == '' || userInput == userInput.defaultValue){
+    console.log("insert some text");
+  }else {
+    fieldN.innerText = (userInput.value);
+
+    createInput.setAttribute("id", userInput.value);
+    divContainer.appendChild(fieldN);
+    divContainer.appendChild(createInput);
+
+    document.querySelector(".form-fields").appendChild(divContainer);
+  }
+}
+
+AddFieldBtn.addEventListener("click", function(){
+  createNewDiv();
+  userInputs = document.querySelectorAll(".form-fields input");
+  if (userInput.value == '' || userInput == userInput.defaultValue){
+    userInput.style.borderColor = "red";
+  }else userInput.style.borderColor = "lightgrey";
+
+  userInput.value = '';
+})
+
