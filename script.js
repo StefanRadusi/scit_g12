0e76646df853bd8093460114510f3662663a606c
iCosteaa <i.ioana3133@yahoo.com>
2020-01-05 17:55:19 +0200

    This is my homework

--- script.js
+++ script.js
@@ -10,3 +10,21 @@ function onClickSendButton() {
 
 // we attached a function to click event triggered by send button
 sendButton.addEventListener("click", onClickSendButton);
+
+document.getElementById("addFiled").addEventListener("click", function(createNewField) {
+  let fieldNameByUser = document.getElementById('fieldName').value;
+  const newDiv = document.createElement("div");
+  newDiv.classList.add("field");
+  const newParagraph = document.createElement("p");
+  const newInput = document.createElement("input");
+  newDiv.appendChild(newParagraph);
+  newDiv.appendChild(newInput);
+  document.getElementById("form").appendChild(newDiv);
+  newParagraph.innerText = fieldNameByUser;
+});
+
+/* function redBorder() {
+  if(getElementsByClassName("field").value.length == "")
+    document.getElementByClassName("field").style.BorderColor="red";
+} */
+
