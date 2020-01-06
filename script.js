b3955c06538d28110ff50b07d86c45958b41afda
Stefan Radusi <stefan.radusi@aeratechnology.com>
2020-01-06 15:17:37 +0200

    teachers comments

--- script.js
+++ script.js
@@ -11,38 +11,34 @@ function onClickSendButton() {
 // we attached a function to click event triggered by send button
 sendButton.addEventListener("click", onClickSendButton);
 
-
-let form = document.getElementById ("addFiled");
+let form = document.getElementById("addFiled");
 // console.log (form);
-let field = document. getElementById ("fields");
+let field = document.getElementById("fields");
 // console.log (fields);
 
-
-form.addEventListener ("click", functionAdd);
-
+form.addEventListener("click", functionAdd);
 
 function functionAdd(e) {
-    e.preventDefault();
-// console.log (1);
-let newItem = document.getElementById ("fieldName").value;
-let div = document.createElement ("div");
-div.className = "field";
-let p = document.createElement("p");
-// console.log (div);
+  e.preventDefault();
+  // console.log (1);
+  let newItem = document.getElementById("fieldName").value;
+  let div = document.createElement("div");
+  div.className = "field";
+  let p = document.createElement("p");
+  // console.log (div);
 
-p.appendChild(document.createTextNode (newItem));
-div.appendChild (p);
+  p.appendChild(document.createTextNode(newItem));
+  div.appendChild(p);
 
-let newInput = document.createElement("input");
+  let newInput = document.createElement("input");
   newInput.setAttribute("type", "text");
   div.appendChild(newInput);
 
-field.appendChild(div);
+  field.appendChild(div);
 
-}
-
-
-if (document.getElementById ("fieldName").value == "") {
-  document.getElementById ("fieldName").style.borderColor = "red";
-
+  //the request was that in the moment of clicking the add filed  button the border will be red if the input is empty
+  // just had to move the code from the global scope to this function scope
+  if (document.getElementById("fieldName").value == "") {
+    document.getElementById("fieldName").style.borderColor = "red";
+  }
 }
