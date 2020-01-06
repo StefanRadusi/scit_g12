a34b5cb9cf2369e989fcccb5766456ce5a06686e
Stefan Radusi <stefan.radusi@aeratechnology.com>
2020-01-06 15:59:09 +0200

    teacher sugestion

--- script.js
+++ script.js
@@ -4,60 +4,54 @@ console.log("start");
 const sendButton = document.getElementById("send");
 const addButton = document.getElementById("addFiled");
 
-
 // this the function that will be called when click event is triggered
 function onClickSendButton() {
-    const form = document.getElementsByClassName("form-fields")[0];
-    const inputList = form.getElementsByTagName("input");
-    let error = false;
-    for (let inputElem of inputList) {
-        if (!inputElem.value) {
-            inputElem.style.borderColor = "red";
-            error = true;
-        } else {
-            inputElem.style.borderColor = null;
-        }
+  let dataString = "Sent info: ";
+  const divFields = document.getElementsByClassName("field");
+  let error = false;
+  for (let i = 0; i < divFields.length; i++) {
+    let pElem = divFields[i].getElementsByTagName("p")[0];
+    let inputElem = divFields[i].getElementsByTagName("input")[0];
+    dataString += `${pElem.innerText} - ${inputElem.value},`;
+    if (!inputElem.value) {
+      inputElem.style.borderColor = "red";
+      error = true;
+    } else {
+      inputElem.style.borderColor = null;
     }
-    if (!error) {
-        let dataString = "Sent info: ";
-        const divFields = document.getElementsByClassName("field");
-        for (let i = 0; i < divFields.length; i++) {
-            let pElem = divFields[i].getElementsByTagName("p")[0];
-            let inputElem = divFields[i].getElementsByTagName("input")[0];
-            dataString += `${pElem.innerText} - ${inputElem.value},`;
-        }
-        document.getElementsByClassName("form-container")[0].innerHTML = dataString;
-    }
+  }
+
+  if (!error) {
+    document.getElementsByClassName("form-container")[0].innerHTML = dataString;
+  }
 }
 
-document.getElementById("fieldName").oninput = function () {
-    this.style.borderColor = null;
+document.getElementById("fieldName").oninput = function() {
+  this.style.borderColor = null;
 };
 
 function addNewField() {
-    const inputField = document.getElementById("fieldName");
-    const fieldName = inputField.value;
-    if (!fieldName) {
-        inputField.style.borderColor = "red";
-    } else {
-        const newDiv = document.createElement("div");
-        newDiv.setAttribute("class", "field");
-        const form = document.getElementsByClassName("form-fields")[0];
-        form.appendChild(newDiv);
+  const inputField = document.getElementById("fieldName");
+  const fieldName = inputField.value;
+  if (!fieldName) {
+    inputField.style.borderColor = "red";
+  } else {
+    const newDiv = document.createElement("div");
+    newDiv.setAttribute("class", "field");
+    const form = document.getElementsByClassName("form-fields")[0];
+    form.appendChild(newDiv);
 
-        const newP = document.createElement("p");
-        newP.innerText = fieldName;
-        newDiv.appendChild(newP);
+    const newP = document.createElement("p");
+    newP.innerText = fieldName;
+    newDiv.appendChild(newP);
 
-        const newInput = document.createElement("input");
-        newInput.id = fieldName;
-        newDiv.appendChild(newInput);
-        inputField.value = null;
-    }
+    const newInput = document.createElement("input");
+    newInput.id = fieldName;
+    newDiv.appendChild(newInput);
+    inputField.value = null;
+  }
 }
 
 // we attached a function to click event triggered by send button
 sendButton.addEventListener("click", onClickSendButton);
 addButton.addEventListener("click", addNewField);
-
-
