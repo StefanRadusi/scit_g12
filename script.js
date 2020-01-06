1bd3c845f0e21ccdb9184b94456877cfdc576ddf
vasiluca32 <vasilelucaluca@gmail.com>
2020-01-06 20:20:58 +0200

    homework resolved

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ function onClickSendButton() {
 
 // we attached a function to click event triggered by send button
 sendButton.addEventListener("click", onClickSendButton);
+
+
+
+function newField() {
+
+  if (document.getElementById("fieldName").value === "") {
+    document.getElementById("fieldName").style.borderColor = "red";
+  } else {
+    document.getElementById("fieldName").style.borderColor = "lightgrey";
+    const newDiv = document.createElement("div");
+    newDiv.className = "field";
+
+    const pContent = document.createElement("p");
+    pContent.innerText = document.getElementById("fieldName").value;
+    newDiv.appendChild(pContent);
+
+    const input = document.createElement("input");
+    newDiv.appendChild(input);
+
+    document.getElementById("form-fields").appendChild(newDiv);
+  }
+  document.getElementById("fieldName").value = "";
+
+}
+
