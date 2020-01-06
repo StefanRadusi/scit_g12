661672d1e7740adedffa01e375fb77150b2fc31c
GuranMaria <guranmmaria@yahoo.com>
2020-01-06 20:59:42 +0200

    homework done

--- script.js
+++ script.js
@@ -1,3 +1,51 @@
 console.log("start");
 
-let a = 1;
+// example of reacting to button click
+const sendButton = document.getElementById("send");
+const addButton = document.getElementById("addFiled");
+
+// this the function that will be called when click event is triggered
+function onClickSendButton() {
+  console.log("click on", sendButton);
+}
+
+document.getElementById("fieldName").oninput = function() {
+  this.style.borderColor = null;
+};
+
+function addNewField() {
+  const inputField = document.getElementById("fieldName");
+  const fieldName = inputField.value;
+  if (!fieldName) {
+      inputField.style.borderColor = "red";
+  } else {
+      const newDiv = document.createElement("div");
+      newDiv.setAttribute("class", "field");
+      const form = document.getElementsByClassName("form-fields")[0];
+      form.appendChild(newDiv);
+
+      const newP = document.createElement("p");
+      newP.innerText = fieldName;
+      newDiv.appendChild(newP);
+
+      const newInput = document.createElement("input");
+      newInput.id = fieldName;
+      newDiv.appendChild(newInput);
+  }
+}
+ 
+// we attached a function to click event triggered by send button
+sendButton.addEventListener("click", onClickSendButton);
+addButton.addEventListener("click", addNewField);
+ 
+function returnForm(){
+  if (!field && !form-container){
+  inputField.style.borderColor = "red";
+}
+else {
+  function myFunction() {
+    var x = document.getElementById("field").autocomplete;
+    document.getElementById("form-container").innerHTML = x;
+  }
+}
+};
