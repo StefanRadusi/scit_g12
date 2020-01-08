dcf5afff96658ac507830251aab6eb676421acb8
adrianklivecz <adrian.clivet@yahoo.com>
2020-01-08 18:25:03 +0200

    working progress

--- script.js
+++ script.js
@@ -10,3 +10,28 @@ function onClickSendButton() {
 
 // we attached a function to click event triggered by send button
 sendButton.addEventListener("click", onClickSendButton);
+
+
+// 1. 
+const newField = document.getElementById("addField");
+
+const fields = document.getElementsByClassName("form-fields");
+
+function onClickAddField() {
+  const newDiv = document.createElement("div");
+  newDiv.className = "field";
+  for(let i = 0; i < newDiv.length; i++) {
+    document.getElementsByClassName("field").appendChild(newDiv[i]);
+  }
+  const newParagraph = document.createElement("p");
+  newDiv.appendChild(newParagraph);
+
+  const newInput = document.createElement("input");
+  newInput.id = "fieldName";
+  newDiv.appendChild(newInput);
+  // console.log(newDiv);
+}
+
+newField.addEventListener("click", onClickAddField);
+
+
