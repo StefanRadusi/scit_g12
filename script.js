4fc899f8fe24ccafc914bdb418c3de7282e51e35
dianastetco <stetcod@yahoo.com>
2020-01-13 19:50:48 +0200

    finish

--- script.js
+++ script.js
@@ -10,3 +10,33 @@ function onClickSendButton() {
 
 // we attached a function to click event triggered by send button
 sendButton.addEventListener("click", onClickSendButton);
+
+// 1.
+const fieldName = document.getElementById("fieldName");
+function addNewField() {
+  const fieldNameValue = fieldName.value;
+  fieldName.value = "";
+  fieldName.style.borderColor = "lightgray"; 
+
+// 2.
+  if (fieldNameValue) {
+
+  const div = document.createElement("div");
+    div.classList.add("field");
+
+    const p = document.createElement("p");
+    p.innerText = fieldNameValue;
+    div.appendChild(p);
+
+    const input = document.createElement("input");
+    input.id = fieldNameValue;
+    input.classList.add("field-input");
+    div.appendChild(input);
+
+    document.getElementById("form-fields").appendChild(div);
+  } else {
+    fieldName.style.borderColor = "red";
+  }
+}
+
+document.getElementById("addFiled").addEventListener("click", addNewField);
