fe79e0f8e316abb9868d03817a23fdb6d1310bb4
Delia Popescu <deliabuze@gmail.com>
2020-01-14 15:03:04 +0200

    clock

--- script.js
+++ script.js
@@ -16,20 +16,20 @@ console.log("start");
 
 // console.log(a);
 
-function createParagraph(text) {
-  console.log(text);
-  const p = document.createElement("p");
-  p.innerText = text;
+// function createParagraph(text) {
+//   console.log(text);
+//   const p = document.createElement("p");
+//   p.innerText = text;
 
-  document.getElementById("body").appendChild(p);
+//   document.getElementById("body").appendChild(p);
 
-  return p;
-}
+//   return p;
+// }
 
-function updateParagraphText(text, p) {
-  console.log(p);
-  p.innerText = text;
-}
+// function updateParagraphText(text, p) {
+//   console.log(p);
+//   p.innerText = text;
+// }
 
 // function handleInput(event) {
 //   if (event.key === "Enter") {
@@ -41,18 +41,50 @@ function updateParagraphText(text, p) {
 //   }
 // }
 
-document.getElementById("text").addEventListener("keydown", handleInput);
+// document.getElementById("text").addEventListener("keydown", handleInput);
 
-const p = createParagraph(0);
-let a = 0;
-function handleInput(event) {
-  if (event.key === "Enter") {
-    a = a + 1;
-    console.log(a);
-    updateParagraphText(a, p);
-  }
-}
+// const p = createParagraph(0);
+// let a = 0;
+// function handleInput(event) {
+//   if (event.key === "Enter") {
+//     a = a + 1;
+//     console.log(a);
+//     updateParagraphText(a, p);
+//   }
+// }
 
-setInterval(function() {
-  console.log("run interval function");
-}, 1000);
+// setInterval(function() {
+//   console.log("run interval function");
+// }, 1000);
+
+let hours=0;
+let minutes=0;
+let seconds=0;
+const secondP = document.getElementById("seconds");
+const minutesP = document.getElementById("minutes");
+const hoursP = document.getElementById("hours");
+
+setInterval(function(){
+  seconds++;
+ let s =document.getElementById("seconds");
+ console.log(s.innerText);
+ if (seconds>59)
+ {
+   seconds=0;
+   minutes++;
+   
+ }
+ if(minutes>59)
+ {
+   minutes=0;
+   hours++;
+ }
+ if(hours>24){
+   hours=0;
+ }
+ console.log(hours,minutes,seconds);
+ secondP.innerText=seconds < 10 ? "0" + seconds : seconds;
+ minutesP.innerText=minutes < 10 ? "0" + minutes : minutes;
+ hoursP.innerText=hours < 10 ? "0" + hours : hours;
+
+}, 1);
