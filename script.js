f58201ec005446517013a52383a3bca56fdf76b6
Stefan Radusi <stefan.radusi@aeratechnology.com>
2020-01-27 18:12:11 +0200

    promises

--- script.js
+++ script.js
@@ -17,27 +17,60 @@ function renderText(nrOfSeconds) {
 }
 
 // this function will start a timer
-function startTimer(value) {
-  // the value from input is a string an we need to transform in number
-  // because timeout function has as second parameter milliseconds
-  const delay = Number(value) * 1000;
-  console.log(delay);
+// function startTimer(value) {
+//   // the value from input is a string an we need to transform in number
+//   // because timeout function has as second parameter milliseconds
+//   const delay = Number(value) * 1000;
+//   console.log(delay);
 
-  // in case the value from the input is not convertible to a number t6he result of "Number(value)" will be "NaN";
-  // in the case of "NaN" we don't want to call setTimeout because the delay parameter is not valid
-  if (delay != NaN && delay > 0) {
-    setTimeout(function() {
-      renderText(Number(value));
-    }, delay);
-  }
+//   // in case the value from the input is not convertible to a number t6he result of "Number(value)" will be "NaN";
+//   // in the case of "NaN" we don't want to call setTimeout because the delay parameter is not valid
+//   if (delay != NaN && delay > 0) {
+//     setTimeout(function() {
+//       renderText(Number(value));
+//     }, delay);
+//   }
+// }
+
+function handleRejection(text) {
+  const p = document.createElement("p");
+  p.innerText = text;
+
+  document.getElementById("text-container").appendChild(p);
 }
 
+function startTimer(value) {
+  const delay = Number(value) * 1000;
+  console.log(delay);
+  return new Promise(function(resolve, reject) {
+    if (delay != NaN && delay > 0) {
+      setTimeout(function() {
+        resolve(Number(value));
+      }, delay);
+    } else {
+      reject("Please enter valid number");
+    }
+  });
+}
+
+function handleEnterOnInput() {
+  return new Promise(function(resolve, reject) {
+    document
+      .getElementById("text")
+      .addEventListener("keydown", function(event) {
+        if (event.key === "Enter") {
+          resolve(event.currentTarget.value);
+        }
+      });
+  });
+}
+
+handleEnterOnInput()
+  .then(startTimer)
+  .then(renderText)
+  .catch(handleRejection);
+
 // we react to enter key on input
-document.getElementById("text").addEventListener("keydown", function(event) {
-  if (event.key === "Enter") {
-    startTimer(event.currentTarget.value);
-  }
-});
 
 // ----- example 2: Create a timer with "setInterval" builtin function ------
 
@@ -81,3 +114,56 @@ setInterval(function() {
 
   //the timer tracks seconds, so in order for the callback function to be called every second we need to set "setInterval" to 1000 milliseconds
 }, 1000);
+
+function handleClickToFetch() {
+  return new Promise(function(resolve) {
+    document.getElementById("fetch-meal").addEventListener("click", function() {
+      resolve();
+    });
+  });
+}
+
+function parseBody(response) {
+  return response.json();
+}
+
+function fetchData() {
+  console.log("fetch");
+
+  return new Promise(function(resolve, reject) {
+    showMealTitle("loading");
+    fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
+      .then(parseBody)
+      .then(function(json) {
+        console.log(json);
+        resolve(json.meals[0].strMeal);
+      })
+      .catch(function() {
+        reject("Something went wrong");
+      });
+  });
+}
+
+function showMealTitle(mealTitle) {
+  document.getElementById("meal").innerText = mealTitle;
+}
+
+handleClickToFetch()
+  .then(fetchData)
+  .then(showMealTitle)
+  .catch(showMealTitle);
+
+document.getElementById("fetch-meal").addEventListener("click", function() {
+  showMealTitle("loading");
+  fetch("https://www.themealdb.com/api/json/v1/1/search.php?s=Arrabiata")
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(json) {
+      console.log(json);
+      showMealTitle(json.meals[0].strMeal);
+    })
+    .catch(function() {
+      showMealTitle("Something went wrong");
+    });
+});
