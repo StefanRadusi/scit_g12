a496295edde00dd69e66a1f28c42dfcca6781f2c
Stefan Radusi <stefan.radusi@aeratechnology.com>
2020-02-03 20:57:22 +0200

    class code

---
+++ carousel.js
@@ -0,0 +1,37 @@
+class Carousel {
+  constructor() {
+    this.generateContainer();
+    this.imgDomList = [];
+  }
+
+  generateContainer() {
+    this.container = document.createElement("div");
+    this.container.classList.add("carousel-container");
+
+    this.containerImgs = document.createElement("div");
+    this.containerImgs.classList.add("carousel-img-container");
+
+    document.body.appendChild(this.container);
+    this.container.appendChild(this.containerImgs);
+  }
+
+  setImgUrls(urls) {
+    this.urls = urls;
+    // console.log(this.urls);
+    this.generateImgDom();
+  }
+
+  generateImgDom() {
+    for (let i = 0; i < 3; i++) {
+      const url = this.urls[i];
+      console.log(url);
+
+      const img = document.createElement("img");
+      img.setAttribute("src", url);
+      img.classList.add("img-carousel");
+      this.containerImgs.appendChild(img);
+
+      this.imgDomList.push(img);
+    }
+  }
+}
---
+++ fetchData.js
@@ -0,0 +1,45 @@
+class FetchData {
+  constructor(carousel) {
+    this.createInput();
+    this.setOnEnter();
+    this.carousel = carousel;
+  }
+
+  createInput() {
+    const input = document.createElement("input");
+    document.body.prepend(input);
+    this.inputRef = input;
+  }
+
+  setOnEnter() {
+    this.inputRef.addEventListener("keydown", this.handleEnter.bind(this));
+  }
+
+  handleEnter(event) {
+    if (event.key === "Enter") {
+      this.fetchRecipes();
+    }
+  }
+
+  fetchRecipes() {
+    const url = this.generateUrl();
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(this.generateImgUrl.bind(this));
+  }
+
+  generateImgUrl(json) {
+    this.imgUrls = json.meals.map(function(element) {
+      return element.strMealThumb;
+    });
+
+    this.carousel.setImgUrls(this.imgUrls);
+  }
+
+  generateUrl() {
+    const value = this.inputRef.value;
+    return `https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`;
+  }
+}
