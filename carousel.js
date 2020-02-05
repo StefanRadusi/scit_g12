5222f06c9506b68f2e3f27df0297e1fab5a84148
hedi31 <heinrichhedviganiko@gmail.com>
2020-02-05 20:55:03 +0200

    v1

--- carousel.js
+++ carousel.js
@@ -5,7 +5,7 @@ class Carousel {
   }
 
   generateContainer() {
-    this.container = document.createElement("div");
+    this.container = document.createElement("div");  //this.container este un atribut creat in metoda obiectului.
     this.container.classList.add("carousel-container");
 
     this.containerImgs = document.createElement("div");
@@ -22,16 +22,17 @@ class Carousel {
   }
 
   generateImgDom() {
-    for (let i = 0; i < 3; i++) {
+    this.containerImgs.innerHTML = ""; //task1
+        for (let i = 0; i < 3; i++) {
+          if (this.urls[i]){ //task2
       const url = this.urls[i];
       console.log(url);
-
       const img = document.createElement("img");
       img.setAttribute("src", url);
       img.classList.add("img-carousel");
       this.containerImgs.appendChild(img);
 
       this.imgDomList.push(img);
-    }
+    }}
   }
 }
--- fetchData.js
+++ fetchData.js
@@ -8,14 +8,14 @@ class FetchData {
   createInput() {
     const input = document.createElement("input");
     document.body.prepend(input);
-    this.inputRef = input;
+    this.inputRef = input; //retinem elementul DOM pentru a optimizia, adica sa nu tot facem getElementByID
   }
 
-  setOnEnter() {
+  setOnEnter() { //sincron
     this.inputRef.addEventListener("keydown", this.handleEnter.bind(this));
   }
 
-  handleEnter(event) {
+  handleEnter(event) { //asincron
     if (event.key === "Enter") {
       this.fetchRecipes();
     }
@@ -23,13 +23,14 @@ class FetchData {
 
   fetchRecipes() {
     const url = this.generateUrl();
-    fetch(url)
+    fetch(url) 
       .then(function(response) {
         return response.json();
       })
-      .then(this.generateImgUrl.bind(this));
+      .then(this.generateImgUrl.bind(this)); //cand se parseaza raspunsul 
   }
-
+  
+  
   generateImgUrl(json) {
     this.imgUrls = json.meals.map(function(element) {
       return element.strMealThumb;
@@ -41,5 +42,11 @@ class FetchData {
   generateUrl() {
     const value = this.inputRef.value;
     return `https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`;
+    // generateButtons: ce din stanga din primma sa fie disabeld- startpoint lenght of listurls-3 
+    // la apasare pe drepta imagina 2 in loc la 1 si 3 in loc la 2 si la 3 apare una noua-alta metoda
+    // scimbam src corect cu imageDOMElements
+    // trebuie sa setam start pint cu zwro si la apasare devine 1- 
+    // cu un for iteram prin cele 3 si returnezdi din lista de urluri pornind de la list.strartpoint[3]
+    // la stanga startpointul se decrementam(incepem de tot de la 3) 
   }
 }
