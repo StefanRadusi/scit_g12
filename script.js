e73d537ac7650b5f2cd0c581493a3cbb1d67ee9c
unknown <cebotari.luminita1@gmail.com>
2020-02-09 20:26:35 +0200

    homework O&C

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ function HangMan() {
   this.word = words[Math.floor(Math.random() * words.length)];
   this.lettersDOM = [];
   this.mistakes = 0;
+  this.lettersFound = 0;
 }
 
 HangMan.prototype.renderUnderScores = function() {
@@ -45,21 +46,66 @@ HangMan.prototype.matchLetter = function(letter) {
     if (wordLetter === letter) {
       const p = this.lettersDOM[index];
       p.innerText = letter;
+      this.lettersFound++;
     }
+  }
+
+  if (this.lettersFound === this.word.length) {
+    this.handleWin();
   }
 };
 
 HangMan.prototype.updateMistakes = function() {
   const currentMistake = this.mistakes;
-  this.mistakes = this.mistakes + 1;
+  this.mistakes = this.mistakes + 1; 
 
   const mistakes = document.getElementById("mistakes");
   const currentText = mistakes.innerText;
   const newText = currentText.replace(currentMistake, this.mistakes);
   mistakes.innerText = newText;
+  this.handelLost(this.mistakes, mistakes);
 };
+
+HangMan.prototype.handelLost = function(mistake, mistakes) {
+  if (mistake === 3) {
+    mistakes.innerText = "You lost"
+    for (const [index, wordLetter] of this.word.split("").entries()) {
+      const p = this.lettersDOM[index];
+        p.innerText = wordLetter;
+    }
+  }}
+
+  HangMan.prototype.handleWin = function() {
+    const mistakes = document.getElementById("mistakes");
+    mistakes.innerText = "You win";
+    document.getElementById("input").disabled = true;
+  }
+  
+
+  HangMan.prototype.resetGame = function() {
+    const obj = this;
+    document.getElementById("reset").addEventListener("click",function(){ 
+    document.getElementById("input").disabled = false;
+    obj.counter = 0;
+    obj.lettersDOM = [];
+    obj.mistakes = 0;
+    
+    let element = document.getElementById("letters");
+    while (element.firstChild) {
+    element.removeChild(element.firstChild);
+    }
+    const mistakes = document.getElementById("mistakes");
+    mistakes.innerText = "You have 0 mistakes";
+    obj.renderUnderScores();
+    obj.getInputFromUser();
+    });
+      
+  }
+  
+
 
 const hangMan = new HangMan();
 hangMan.renderUnderScores();
 hangMan.getInputFromUser();
-console.log(hangMan);
+hangMan.resetGame();
+console.log(hangMan); 
