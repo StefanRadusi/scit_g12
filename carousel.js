4e994dc54a1936126ee9c1f0df258f21a79a613d
Flaviu-Sofran <s_flaviu91@yahoo.com>
2020-02-10 20:24:33 +0200

    carousel change

--- carousel.js
+++ carousel.js
@@ -22,16 +22,22 @@ class Carousel {
   }
 
   generateImgDom() {
+    this.containerImgs.innerHTML = null;
+    this.imgDomList = [];
     for (let i = 0; i < 3; i++) {
       const url = this.urls[i];
       console.log(url);
+      if ( url) {
+      
 
       const img = document.createElement("img");
       img.setAttribute("src", url);
       img.classList.add("img-carousel");
       this.containerImgs.appendChild(img);
+      
 
       this.imgDomList.push(img);
+      }
     }
   }
 }
