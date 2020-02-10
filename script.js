9f1aff05c06f80e486b9c5d9c6911248e5323073
Boo1122 <bbb_slv@hotmail.co.uk>
2020-02-11 01:48:48 +0200

    WORK

--- script.js
+++
@@ -1,9 +0,0 @@
-const carousel = new Carousel();
-const fetchData = new FetchData(carousel);
-
-// alt + sageata muta codul sus sau jos
-
-// arrow function e mereu anonima
-
-// resursele se cache-uiesc de catre browser
-
