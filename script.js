232e8c61e2d0079c049f29528e2464e7e6edad96
George4888 <DanGeorge4888@gmail.com>
2020-02-12 19:52:24 +0200

    WORKS

--- script.js
+++ script.js
@@ -1,3 +1,64 @@
-console.log("start");
+class TheMover {
+  constructor() {
+    this.getDot();
+    this.pressLeftArrow();
+    this.pressUpArrow();
+    this.pressRightArrow();
+    this.pressDownArrow();
+    this.positionX = 0;
+    this.positionY = 0;
+  }
 
-let a = 1;
+  getDot() {
+    const whiteDot = document.getElementById("whiteDot");
+    this.wDot = whiteDot;
+  }
+
+  pressLeftArrow() {
+    document.addEventListener("keydown", this.leftArrow);
+  }
+
+  leftArrow = event => {
+    console.log(event);
+    if (event.keyCode === 37) {
+      this.positionX -= 1;
+      this.wDot.style.left = `${this.positionX}px`;
+      console.log(this.positionX);
+    }
+  };
+
+  pressUpArrow() {
+    document.addEventListener("keydown", this.upArrow);
+  }
+
+  upArrow = event => {
+    if (event.keyCode === 38) {
+      this.positionY -= 1;
+      this.wDot.style.top = `${this.positionY}px`;
+    }
+  };
+
+  pressRightArrow() {
+    document.addEventListener("keydown", this.rightArrow);
+  }
+
+  rightArrow = (event, pixel) => {
+    if (event.keyCode === 39) {
+      this.positionX += 1;
+      this.wDot.style.left = `${this.positionX}px`;
+    }
+  };
+
+  pressDownArrow() {
+    document.addEventListener("keydown", this.downArrow);
+  }
+
+  downArrow = (event, pixel) => {
+    if (event.keyCode === 40) {
+      this.positionY += 1;
+      this.wDot.style.top = `${this.positionY}px`;
+    }
+  };
+}
+
+const startGame = new TheMover();
