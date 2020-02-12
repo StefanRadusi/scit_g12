87ad1d55c2901318c0a7c266ea986448d18a0eb0
Boo1122 <bbb_slv@hotmail.co.uk>
2020-02-12 20:57:08 +0200

    YESS!!

--- script.js
+++ script.js
@@ -1,18 +1,82 @@
+
 class TheMover {
-    constructor(width, height, a, b ) {
-        this.width = width;
-        this.height = height;
-        this.a = a;
-        this.b = b;
+    constructor() {
+        this.moverDom = this.getMoverDom();
+        this.pressArrowleft();
+        this.pressArrowRight();
+        this.pressArrowDown()
+        this.pressArrowUp();
+        this.positionY = 0;
+        this.positionX = 0;
+    
     }
 
-    speed() {
-        this.a += this.speedA;
-        this.b += this.speedB;
+    getMoverDom() {
+        return document.getElementById("mover");
+    }
+    
+    pressArrowleft() {
+        
+        document.addEventListener("keydown", this.arrowLeft);
+    }
+
+
+    arrowLeft = (event) => {
+        if(event.keyCode === 37) {
+
+            this.positionX -=5;
+            this.moverDom.style.left = `${this.positionX}px`;
+            if(this.positionX === -5) {
+                this.positionX = 0;
+            }
+          
+        }
+    }
+
+    pressArrowRight() {
+        
+        document.addEventListener("keydown", this.arrowRight);
+    }
+
+    arrowRight = (event) => {
+        if(event.keyCode === 39) {
+            this.positionX +=5;
+            this.moverDom.style.left = `${this.positionX}px`;
+        }
+      
+
+    }
+
+    pressArrowDown() {
+        document.addEventListener("keydown", this.arrowDown);
+    }
+
+    arrowDown = (event) => {
+        if(event.keyCode === 40) {
+            this.positionY +=5;
+            this.moverDom.style.top = `${this.positionY}px`;
     }
 }
 
+    pressArrowUp() {
+    
+        document.addEventListener("keydown", this.arrowUp);
+    }
+    
+    arrowUp = () => {
+        if(event.keyCode === 38) {
+            this.positionY -=5;
+            this.moverDom.style.top = `${this.positionY}px`;
+            
+            if(this.positionY === -5) {
+                this.positionY = 0;
+            }
+        }
 
+    }
+}
+
+    let theMover = new TheMover();
 
 
 
