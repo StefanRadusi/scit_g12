b01db00fa5fa96bc3b2aa88a58291e46c62855e7
Boo1122 <bbb_slv@hotmail.co.uk>
2020-02-13 10:56:02 +0200

    a

--- script.js
+++ script.js
@@ -6,13 +6,33 @@ class TheMover {
         this.pressArrowRight();
         this.pressArrowDown()
         this.pressArrowUp();
+        this.obstaclesDom = this.getObstacles();
+        this.obstacleOne();
+        this.obstacleTwo();
         this.positionY = 0;
         this.positionX = 0;
+        
+        
     
     }
 
-    getMoverDom() {
+    getMoverDom = () => {
         return document.getElementById("mover");
+    }
+
+    pressArrowUp() {
+
+        document.addEventListener("keydown", this.arrowUp);
+    }
+    
+    arrowUp = () => {
+        if(event.keyCode === 38) {
+            if(this.positionY <=0) {
+                this.positionY +=5;
+            }
+            this.positionY -=5;
+            this.moverDom.style.top = `${this.positionY}px`;
+        }
     }
     
     pressArrowleft() {
@@ -35,11 +55,11 @@ class TheMover {
 
     arrowRight = (event) => {
         if(event.keyCode === 39) {
-            this.positionX +=5;
-            this.moverDom.style.left = `${this.positionX}px`;
             if(this.positionX >=680) {
                 this.positionX -=5;
             }
+            this.positionX +=5;
+            this.moverDom.style.left = `${this.positionX}px`;
         }
     }
 
@@ -52,31 +72,44 @@ class TheMover {
             this.positionY +=5;
             this.moverDom.style.top = `${this.positionY}px`;
             if(this.positionY >=330) {
-                this.positionY-=5;
-            }
+                this.positionY-=5; 
+         }
     }
 }
 
-    pressArrowUp() {
-    
-        document.addEventListener("keydown", this.arrowUp);
-    }
-    
-    arrowUp = () => {
-        if(event.keyCode === 38) {
-            if(this.positionY <=0) {
-                this.positionY +=5;
-            }
-            this.positionY -=5;
-            this.moverDom.style.top = `${this.positionY}px`;
-            
-            
-        }
-
-    }
+getObstacles = () => {
+   return document.getElementsByClassName("obstacle");
 }
+
+obstacleOne = () => {
+    document.addEventListener("keydown", this.o1);
+}
+o1 = (event) => {
+   
+ }
+
+ obstacleTwo = () => {
+    document.addEventListener("keydown", this.o2);
+}
+o2 = (event) => {
+   
+    // if(event.keyCode === 39 ) {
+    //     this.positionX-=5;
+        
+    //     }
+ }
+
+
+
+
+ 
+}
+
+    
+
 
     let theMover = new TheMover();
+    
 
 
 
