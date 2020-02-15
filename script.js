5085813df0ed9c34adcc119e225c8b2e1cad3813
Stefan Radusi <stefan.radusi@aeratechnology.com>
2020-02-15 15:17:57 +0200

    teacher solution

--- script.js
+++ script.js
@@ -5,8 +5,6 @@ const DEFAULT_MOVE_INCREMENT = 2;
 class Player {
   constructor() {
     this.domElement = document.getElementById("player");
-    this.obstacles = document.getElementsByClassName("obstacle");
-
     // here we store the initial position of the player
     this.top = 0;
     this.left = 0;
@@ -18,16 +16,20 @@ class Player {
   setMovement() {
     // because we are interested in reacting to keyboard press on the window in general not on a specific DOM element, we will set an event listener on document
     // we must use bind so that "this" will refer (in the "movePlayer" method) to "Player"  object otherwise "this" would refer to "document" object because there is the context on which we added the event listener
-    document.addEventListener("keydown", this.movePlayer.bind(this));
+    document.addEventListener("keydown", this.movePlayer);
   }
 
-  movePlayer(event) {
+  movePlayer = event => {
     // event is the default parameter that it is passed to any callback function of an "addEventListener"
     // event is a complex object that contains information regarding the event that was trigger by browser
     // in this case the event is of type "keydown" but we need to react only to some type "keydown" event
     switch (event.key) {
       case "ArrowDown":
         this.top = this.top + DEFAULT_MOVE_INCREMENT;
+
+        // we move the white div which is out player by changing it position
+        // the payer is initial positions at top = 0 and left = 0
+        // if we change this style attributes on the dom elements then we change the position in the window thus making the affect of moving a element
         this.domElement.style.top = this.top + "px";
         break;
       case "ArrowUp":
@@ -46,16 +48,7 @@ class Player {
       default:
         break;
     }
-
-    this.checkIfHitObstacle();
-  }
-
-  checkIfHitObstacle() {
-    for (const obstacle of this.obstacles) {
-      const { left: obstacleX } = obstacle.getBoundingClientRect();
-      console.log(obstacleX);
-    }
-  }
+  };
 }
 
 new Player();
