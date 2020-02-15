05478ee69540f796a3f85e794f566288ebf0adc2
hedi31 <heinrichhedviganiko@gmail.com>
2020-02-15 20:11:30 +0200

    first-part

--- carousel.js
+++ carousel.js
@@ -2,6 +2,8 @@ class Carousel {
   constructor() {
     this.generateContainer();
     this.imgDomList = [];
+    this.startPoint = 0;
+    
   }
 
   generateContainer() {
@@ -19,6 +21,8 @@ class Carousel {
     this.urls = urls;
     // console.log(this.urls);
     this.generateImgDom();
+    this.generateButtons();
+    
   }
 
   generateImgDom() {
@@ -35,4 +39,47 @@ class Carousel {
       this.imgDomList.push(img);
     }}
   }
+
+//homework
+
+  generateButtons() {
+    this.cleanGeneratebutton();
+    this.leftButton = document.createElement("button");
+    this.leftButton.id = "leftButton";
+    this.leftButton.innerHTML = "<";
+
+    this.rightButton = document.createElement("button");
+    this.rightButton.id = "rightButton";
+    this.rightButton.innerHTML = ">";
+
+    this.container.prepend(this.leftButton);
+    this.container.appendChild(this.rightButton);
+
+    this.switchImage();
+  
+  }
+  cleanGeneratebutton() {
+    if (document.getElementById("leftButton")){
+    document.getElementById("leftButton").remove();
+    document.getElementById("rightButton").remove();
+  }
 }
+
+switchImage(){
+  document.getElementById("rightButton").addEventListener("click", function(){
+    console.log('event listener works');
+    if(this.startPoint === 1){
+
+    }
+  })
+  }
+
+
+
+};
+   // generateButtons: ce din stanga din primma sa fie disabeld- startpoint lenght of listurls-3 
+    // la apasare pe drepta imagina 2 in loc la 1 si 3 in loc la 2 si la 3 apare una noua -alta metoda
+    // scimbam src corect cu imageDOMElements
+    // trebuie sa setam start pint cu zero si la apasare devine 1- 
+    // cu un for iteram prin cele 3 si returnezdi din lista de urluri pornind de la list.strartpoint[3]
+    // la stanga startpointul se decrementam(incepem de tot de la 3) 
--- fetchData.js
+++ fetchData.js
@@ -30,7 +30,6 @@ class FetchData {
       .then(this.generateImgUrl.bind(this)); //cand se parseaza raspunsul 
   }
   
-  
   generateImgUrl(json) {
     this.imgUrls = json.meals.map(function(element) {
       return element.strMealThumb;
@@ -42,11 +41,20 @@ class FetchData {
   generateUrl() {
     const value = this.inputRef.value;
     return `https://www.themealdb.com/api/json/v1/1/search.php?s=${value}`;
-    // generateButtons: ce din stanga din primma sa fie disabeld- startpoint lenght of listurls-3 
-    // la apasare pe drepta imagina 2 in loc la 1 si 3 in loc la 2 si la 3 apare una noua-alta metoda
-    // scimbam src corect cu imageDOMElements
-    // trebuie sa setam start pint cu zwro si la apasare devine 1- 
-    // cu un for iteram prin cele 3 si returnezdi din lista de urluri pornind de la list.strartpoint[3]
-    // la stanga startpointul se decrementam(incepem de tot de la 3) 
   }
-}
+
+  
+     
+   
+  }
+  
+
+
+
+
+
+
+
+
+ 
+  
