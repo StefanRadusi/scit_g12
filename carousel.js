3a741fa2e8bf03f46557ae8a7da3b08b86cda100
George4888 <DanGeorge4888@gmail.com>
2020-02-16 21:27:26 +0200

    works

--- carousel.js
+++ carousel.js
@@ -4,6 +4,7 @@ class Carousel {
     this.getButtonsLocation();
     this.handleLeftButton();
     this.handleRightButton();
+    this.clearButtons();
     this.imgDomList = [];
     this.startPoint = 0;
   }
@@ -20,16 +21,28 @@ class Carousel {
   }
 
   generateButtons() {
+    this.clearButtons();
     this.leftButton = document.createElement("button");
     this.leftButton.id = "leftButton";
     this.leftButton.innerHTML = "<";
+    this.leftButton.disabled = true;
 
     this.rightButton = document.createElement("button");
     this.rightButton.id = "rightButton";
     this.rightButton.innerHTML = ">";
+    if (this.urls.length <= 3) {
+      this.rightButton.disabled = true;
+    }
 
     this.container.prepend(this.leftButton);
     this.container.appendChild(this.rightButton);
+  }
+
+  clearButtons() {
+    if (document.getElementById("leftButton")) {
+      document.getElementById("leftButton").remove();
+      document.getElementById("rightButton").remove();
+    }
   }
 
   setImgUrls(urls) {
@@ -44,6 +57,20 @@ class Carousel {
       const url = this.urls[i];
 
       if (url) {
+        const img = document.createElement("img");
+        img.setAttribute("src", url);
+        img.classList.add("img-carousel");
+        this.containerImgs.appendChild(img);
+        this.imgDomList.push(img);
+      }
+    }
+  }
+
+  renderNewImgDom() {
+    this.cleanImgsContainer();
+    for (let el of this.urls.slice(this.startPoint, this.startPoint + 3)) {
+      if (el) {
+        const url = el;
         const img = document.createElement("img");
         img.setAttribute("src", url);
         img.classList.add("img-carousel");
@@ -72,7 +99,6 @@ class Carousel {
   previousImg = event => {
     if (event.target.id === "leftButton") {
       event.preventDefault();
-      console.log(event.target);
       this.previousImage();
     }
   };
@@ -84,32 +110,31 @@ class Carousel {
   nextImg = event => {
     if (event.target.id === "rightButton") {
       event.preventDefault();
-      console.log(event.target);
       this.nextImage();
     }
   };
 
   nextImage() {
-    for (this.startPoint of this.urls) {
-      this.imgDomList.push(this.startPoint);
-      this.imgDomList.slice(this.startPoint, 4);
-      console.log(this.startPoint);
-      if (this.startPoint < this.urls.length) {
-        this.startPoint++;
-      }
-      this.generateImgDom();
+    if (this.startPoint < this.urls.length - 3) {
+      this.startPoint++;
+      this.renderNewImgDom();
+    }
+    if (this.startPoint > 0) {
+      document.getElementById("leftButton").disabled = false;
+    }
+    if (this.startPoint === this.urls.length - 3) {
+      document.getElementById("rightButton").disabled = true;
     }
   }
 
   previousImage() {
-    for (this.startPoint of this.urls) {
-      this.imgDomList.push(this.startPoint);
-      this.imgDomList.slice(this.startPoint, 4);
-      console.log(this.startPoint);
-      if (this.startPoint > this.urls.length) {
-        this.startPoint--;
-      }
-      this.generateImgDom();
+    this.startPoint--;
+    this.renderNewImgDom();
+    if (this.startPoint === 0) {
+      document.getElementById("leftButton").disabled = true;
+    }
+    if (this.startPoint < this.urls.length - 3) {
+      document.getElementById("rightButton").disabled = false;
     }
   }
 }
