f20ad25ec0edb64f2b59c2ec615022164c584ce6
unknown <cebotari.luminita1@gmail.com>
2020-02-21 18:21:16 +0200

    first variant

--- components/mealPage/mealsNavigation.js
+++ components/mealPage/mealsNavigation.js
@@ -6,6 +6,7 @@ export function addMealsNavigation(meals, letter, container) {
 
   const leftButton = document.createElement("div");
   leftButton.innerText = "<";
+  leftButton.style.opacity = "0.3"
 
   const rightButton = document.createElement("div");
   rightButton.innerText = ">";
@@ -27,10 +28,39 @@ function setNavigationFunctionality(
 ) {
   let currentIndexMeal = 0;
 
-  leftButton.addEventListener("click", () => {});
-
-  rightButton.addEventListener("click", () => {
-    currentIndexMeal++;
+  leftButton.addEventListener("click", () => {
+    if (currentIndexMeal > 0) {
+      currentIndexMeal--;
+    leftButton.style.opacity = "1";
+    } else 
+    // rightButton.style.opacity = "1"
+    // leftButton.style.opacity = "0.3"
+    leftButton.disabled = "true";
+    
+    
     renderMealsElements(meals[currentIndexMeal], letter, container);
   });
+
+  if (currentIndexMeal === 0) {
+    leftButton.style.opacity = "0.3";
+  };
+  
+
+  rightButton.addEventListener("click", () => {
+    if (currentIndexMeal + 1 < meals.length){
+      currentIndexMeal++;}
+      else {
+        rightButton.disabled = "true";
+      }
+    // } else {
+    //   rightButton.style.opacity = "0.3"
+    // }
+    
+    // leftButton.style.opacity = "1";
+    
+    renderMealsElements(meals[currentIndexMeal], letter, container);
+  });
+  if (currentIndexMeal === meals.length) {
+    rightButton.style.opacity = "0.3";
+  };
 }
