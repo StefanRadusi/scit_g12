fc400481782e00875e3c7c4881b2fea329196683
Stefan Radusi <stefan.radusi@aeratechnology.com>
2020-02-23 14:29:06 +0200

    add explination home page

--- components/homePage/home.js
+++ components/homePage/home.js
@@ -1,13 +1,16 @@
 import { generateMealPage } from "../mealPage/meal";
 
 function generateAlphabetLetters(homePage) {
+  const letterContainer = document.createElement("div");
+  letterContainer.classList.add("letters-container");
   for (let i = 65; i < 91; i++) {
     const letter = String.fromCharCode(i);
     const letterDOM = document.createElement("p");
     letterDOM.innerText = letter;
-    homePage.appendChild(letterDOM);
+    letterContainer.appendChild(letterDOM);
     letterDOM.addEventListener("click", generateMealPage);
   }
+  homePage.appendChild(letterContainer);
 }
 
 export function generateHomePage() {
@@ -15,9 +18,17 @@ export function generateHomePage() {
   homePage.classList.add("home-page");
   document.body.appendChild(homePage);
 
+  addExplanationText(homePage);
   generateAlphabetLetters(homePage);
 }
 
 export function hideHomePage(homePage) {
   homePage.style.display = "none";
+}
+
+function addExplanationText(homePage) {
+  const explanation = document.createElement("h2");
+  explanation.classList.add("explanation");
+  explanation.innerText = "Choose a letter to find meals:";
+  homePage.appendChild(explanation);
 }
