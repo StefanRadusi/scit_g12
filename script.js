2212b802fd25d6f0efdcb8e7e06ffea883553f34
vasiluca32 <vasilelucaluca@gmail.com>
2020-03-01 21:17:42 +0200

    my solution

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ class Player {
     // here we store the initial position of the player
     this.top = 0;
     this.left = 0;
+    this.lives = document.getElementById("lives").innerText;
 
     // here we call the function that sets the behaviour of our player
     this.setMovement();
@@ -48,7 +49,89 @@ class Player {
       default:
         break;
     }
+    this.keepInside();
   };
+  keepInside() {
+    let player = this.domElement;
+    let top = this.top;
+    let left = this.left;
+
+    if (left > 484 && top < 84) {
+      alert("YOU WON!!!");
+      this.positionReset();
+    }
+
+    if (left < 0) {
+      player.style.left = 0 + "px";
+      this.left = 0;
+    }
+
+    if (top < 0) {
+
+      player.style.top = 0 + "px";
+      this.top = 0;
+    }
+    if (top > 284) {
+
+      player.style.top = 284 + "px";
+      this.top = 284;
+    }
+
+    this.handleObstacles(top, left);
+  }
+  handleObstacles(top, left) {
+
+    if (left > 84 && left < 116 && top < 100) { //obstacle 1
+      this.livesCounter();
+    }
+    if (left > 134 && left < 166 && top > 84) { //obstacle 2
+      this.livesCounter();
+    }
+    if (left > 184 && left < 216 && top < 220) { //obstacle 3
+      this.livesCounter();
+    }
+    if (left > 264 && left < 296 && top > 204) { //obstacle 4
+      this.livesCounter();
+    }
+    if (left > 334 && left < 366 && top < 260) { //obstacle 5
+      this.livesCounter();
+    }
+    if (left > 394 && left < 426 && top > 184) { //obstacle 6
+      this.livesCounter();
+    }
+    if (left > 414 && left < 446 && top < 150) { //obstacle 7
+      this.livesCounter();
+    }
+    if (left > 468 && top > 84) { //obstacle 8
+      this.livesCounter();
+    }
+
+  }
+  livesCounter() {
+    let lives = this.lives;
+    lives = this.lives - 1;
+    this.lives = lives;
+    document.getElementById("lives").innerText = lives;
+    alert("YOU CRASHED!!!")
+    this.positionReset();
+    this.handleLives(lives);
+    return lives;
+  }
+  positionReset() {
+    player.style.left = 0 + "px";
+    player.style.top = 0 + "px";
+
+    this.left = 0;
+    this.top = 0;
+  }
+  handleLives(lives) {
+    console.log(lives);
+    if (lives < 0) {
+      alert("GAME OVER");
+      document.getElementById("lives").innerText = 3;
+      this.lives = 3;
+    }
+  }
 }
 
 new Player();
