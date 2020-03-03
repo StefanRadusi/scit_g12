46f54e5cac32fb172b7696277c24a3d98b02b500
Boo1122 <bbb_slv@hotmail.co.uk>
2020-03-03 13:17:43 +0200

    pineapple

--- redirectToArticle.js
+++
@@ -1,8 +0,0 @@
-const listOfArticles = document.getElementsByClassName("article");
-
-for (const article of listOfArticles) {
-  article.addEventListener("click", function() {
-    const articleId = article.id;
-    location.href = articleId + ".html";
-  });
-}
--- script.js
+++
@@ -1,3 +0,0 @@
-console.log("start");
-
-let a = 1;
