2e8c96ee5449c3e45cfbd0ba14f1dcde579252eb
Stefan Radusi <stefan.radusi@aeratechnology.com>
2020-04-14 17:26:27 +0300

    done

--- bundle.js
+++ bundle.js
@@ -86,6 +86,78 @@
 /************************************************************************/
 /******/ ({
 
+/***/ "./components/header/header.js":
+/*!*************************************!*\
+  !*** ./components/header/header.js ***!
+  \*************************************/
+/*! exports provided: createHeader */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"createHeader\", function() { return createHeader; });\n/* harmony import */ var _homeButton__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./homeButton */ \"./components/header/homeButton.js\");\n/* harmony import */ var _mealButton__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./mealButton */ \"./components/header/mealButton.js\");\n\n\n\nfunction createHeader() {\n  const header = document.createElement(\"div\");\n  header.classList.add(\"header\");\n\n  header.appendChild(Object(_homeButton__WEBPACK_IMPORTED_MODULE_0__[\"generateHomeButton\"])());\n  header.appendChild(Object(_mealButton__WEBPACK_IMPORTED_MODULE_1__[\"generateMealButton\"])());\n  document.body.appendChild(header);\n}\n\n\n//# sourceURL=webpack:///./components/header/header.js?");
+
+/***/ }),
+
+/***/ "./components/header/homeButton.js":
+/*!*****************************************!*\
+  !*** ./components/header/homeButton.js ***!
+  \*****************************************/
+/*! exports provided: generateHomeButton */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"generateHomeButton\", function() { return generateHomeButton; });\nfunction generateHomeButton() {\n  const button = document.createElement(\"button\");\n  button.classList.add(\"home-button\");\n  button.classList.add(\"selected\");\n  button.id = \"home-button\";\n  button.innerText = \"Home\";\n  return button;\n}\n\n\n//# sourceURL=webpack:///./components/header/homeButton.js?");
+
+/***/ }),
+
+/***/ "./components/header/mealButton.js":
+/*!*****************************************!*\
+  !*** ./components/header/mealButton.js ***!
+  \*****************************************/
+/*! exports provided: generateMealButton, highlightMealButton */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"generateMealButton\", function() { return generateMealButton; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"highlightMealButton\", function() { return highlightMealButton; });\nfunction generateMealButton() {\n  const button = document.createElement(\"button\");\n  button.classList.add(\"meal-button\");\n  button.id = \"meal-button\";\n  button.innerText = \"Meal\";\n  return button;\n}\n\nfunction highlightMealButton() {\n  document.getElementById(\"meal-button\").classList.add(\"selected\");\n  document.getElementById(\"home-button\").classList.remove(\"selected\");\n}\n\n\n//# sourceURL=webpack:///./components/header/mealButton.js?");
+
+/***/ }),
+
+/***/ "./components/homePage/home.js":
+/*!*************************************!*\
+  !*** ./components/homePage/home.js ***!
+  \*************************************/
+/*! exports provided: generateHomePage, hideHomePage */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"generateHomePage\", function() { return generateHomePage; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"hideHomePage\", function() { return hideHomePage; });\n/* harmony import */ var _mealPage_meal__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../mealPage/meal */ \"./components/mealPage/meal.js\");\n\n\nfunction generateAlphabetLetters(homePage) {\n  const letterContainer = document.createElement(\"div\");\n  letterContainer.classList.add(\"letters-container\");\n  for (let i = 65; i < 91; i++) {\n    const letter = String.fromCharCode(i);\n    const letterDOM = document.createElement(\"p\");\n    letterDOM.innerText = letter;\n    letterContainer.appendChild(letterDOM);\n    letterDOM.addEventListener(\"click\", _mealPage_meal__WEBPACK_IMPORTED_MODULE_0__[\"generateMealPage\"]);\n  }\n  homePage.appendChild(letterContainer);\n}\n\nfunction generateHomePage() {\n  const homePage = document.createElement(\"div\");\n  homePage.classList.add(\"home-page\");\n  document.body.appendChild(homePage);\n\n  addExplanationText(homePage);\n  generateAlphabetLetters(homePage);\n}\n\nfunction hideHomePage(homePage) {\n  homePage.style.display = \"none\";\n}\n\nfunction addExplanationText(homePage) {\n  const explanation = document.createElement(\"h2\");\n  explanation.classList.add(\"explanation\");\n  explanation.innerText = \"Choose a letter to find meals:\";\n  homePage.appendChild(explanation);\n}\n\n\n//# sourceURL=webpack:///./components/homePage/home.js?");
+
+/***/ }),
+
+/***/ "./components/mealPage/meal.js":
+/*!*************************************!*\
+  !*** ./components/mealPage/meal.js ***!
+  \*************************************/
+/*! exports provided: generateMealPage, renderMealsElements */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"generateMealPage\", function() { return generateMealPage; });\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"renderMealsElements\", function() { return renderMealsElements; });\n/* harmony import */ var _homePage_home__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../homePage/home */ \"./components/homePage/home.js\");\n/* harmony import */ var _mealsNavigation__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./mealsNavigation */ \"./components/mealPage/mealsNavigation.js\");\n/* harmony import */ var _header_mealButton__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ../header/mealButton */ \"./components/header/mealButton.js\");\n\n\n\n\nfunction generateMealPage(event) {\n  console.log(\"generating meal page\");\n  Object(_header_mealButton__WEBPACK_IMPORTED_MODULE_2__[\"highlightMealButton\"])();\n  Object(_homePage_home__WEBPACK_IMPORTED_MODULE_0__[\"hideHomePage\"])(event.target.parentNode);\n  getMealsFromServer(event.target.innerText);\n}\n\nfunction getMealsFromServer(letter) {\n  const url = generateUrl(letter);\n  fetch(url)\n    .then(r => r.json())\n    .then(json => {\n      generateMeal(json, letter);\n    });\n  console.log(url);\n}\n\nfunction generateUrl(letter) {\n  return `https://www.themealdb.com/api/json/v1/1/search.php?f=${letter}`;\n}\n\nfunction generateMeal(json, letter) {\n  const meals = json.meals;\n  let mealIndex = 0;\n\n  const container = document.createElement(\"div\");\n  container.classList.add(\"meal-page\");\n  document.body.appendChild(container);\n\n  renderMealsElements(meals[mealIndex], letter, container);\n  Object(_mealsNavigation__WEBPACK_IMPORTED_MODULE_1__[\"addMealsNavigation\"])(meals, letter, container);\n}\n\nfunction renderMealsElements(mealData, letter, container) {\n  container.innerHTML = null;\n\n  const title = document.createElement(\"h1\");\n  title.innerText = `Meal that start with: ${letter}`;\n  container.appendChild(title);\n\n  const mealName = document.createElement(\"h2\");\n  mealName.innerText = `Name: ${mealData.strMeal}`;\n  container.appendChild(mealName);\n\n  const mealImg = document.createElement(\"img\");\n  mealImg.setAttribute(\"src\", mealData.strMealThumb);\n  mealImg.classList.add(\"meal-img\");\n  container.appendChild(mealImg);\n\n  const mealDesc = document.createElement(\"p\");\n  mealDesc.innerText = mealData.strInstructions;\n  container.appendChild(mealDesc);\n}\n\n\n//# sourceURL=webpack:///./components/mealPage/meal.js?");
+
+/***/ }),
+
+/***/ "./components/mealPage/mealsNavigation.js":
+/*!************************************************!*\
+  !*** ./components/mealPage/mealsNavigation.js ***!
+  \************************************************/
+/*! exports provided: addMealsNavigation */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"addMealsNavigation\", function() { return addMealsNavigation; });\n/* harmony import */ var _meal__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./meal */ \"./components/mealPage/meal.js\");\n\n\nfunction addMealsNavigation(meals, letter, container) {\n  const navigation = document.createElement(\"div\");\n  navigation.classList.add(\"meal-navigation\");\n\n  const leftButton = document.createElement(\"div\");\n  leftButton.innerText = \"<\";\n  leftButton.style.opacity = \"0.5\";\n\n  const rightButton = document.createElement(\"div\");\n  rightButton.innerText = \">\";\n\n  navigation.appendChild(leftButton);\n  navigation.appendChild(rightButton);\n\n  document.body.appendChild(navigation);\n\n  setNavigationFunctionality(meals, leftButton, rightButton, letter, container);\n}\n\nfunction setNavigationFunctionality(\n  meals,\n  leftButton,\n  rightButton,\n  letter,\n  container\n) {\n  let currentIndexMeal = 0;\n\n  leftButton.addEventListener(\"click\", () => {\n    if (currentIndexMeal > 0) {\n      currentIndexMeal--;\n      Object(_meal__WEBPACK_IMPORTED_MODULE_0__[\"renderMealsElements\"])(meals[currentIndexMeal], letter, container);\n      rightButton.style.opacity = 1;\n    }\n\n    if (currentIndexMeal === 0) {\n      leftButton.style.opacity = 0.5;\n    }\n  });\n\n  rightButton.addEventListener(\"click\", () => {\n    if (currentIndexMeal + 1 < meals.length) {\n      currentIndexMeal++;\n      Object(_meal__WEBPACK_IMPORTED_MODULE_0__[\"renderMealsElements\"])(meals[currentIndexMeal], letter, container);\n      leftButton.style.opacity = 1;\n    }\n\n    if (currentIndexMeal === meals.length - 1) {\n      rightButton.style.opacity = 0.5;\n    }\n  });\n}\n\n\n//# sourceURL=webpack:///./components/mealPage/mealsNavigation.js?");
+
+/***/ }),
+
 /***/ "./main.js":
 /*!*****************!*\
   !*** ./main.js ***!
@@ -94,19 +166,7 @@
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _script__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./script */ \"./script.js\");\n\n\n// console.log(factorialRecursive(4));\nconsole.log(\"start\");\nconsole.log(Object(_script__WEBPACK_IMPORTED_MODULE_0__[\"factorialRecursive\"])(4));\n\n\n//# sourceURL=webpack:///./main.js?");
-
-/***/ }),
-
-/***/ "./script.js":
-/*!*******************!*\
-  !*** ./script.js ***!
-  \*******************/
-/*! exports provided: factorialRecursive */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export (binding) */ __webpack_require__.d(__webpack_exports__, \"factorialRecursive\", function() { return factorialRecursive; });\n// console.log(\"start\");\n\nfunction factorial(n) {\n  if (n === 0) return 0;\n\n  let result = 1;\n  for (let i = 1; i <= n; i++) {\n    result = result * i;\n  }\n\n  return result;\n}\n\n// console.log(factorial(4));\n\nfunction factorialRecursive(n) {\n  if (n === 0) return 0;\n  if (n === 1) return 1;\n  factorialRecursive(n - 1);\n}\n\n// const array = [1, 2, 3];\nfunction goTroughArray(array, index) {\n  const element = array[index];\n  if (array.length === index) return null;\n  console.log(element);\n  goTroughArray(array, index + 1);\n}\n\n// goTroughArray(array, 0);\n\n// const resultFactorialRecursive = factorialRecursive(4);\n// console.log(resultFactorialRecursive);\n\n// fetch(\"https://www.themealdb.com/api/json/v1/1/search.php?s=a\")\n//   .then(r => r.json())\n//   .then(json => {\n//     console.log(json);\n//     const obj = searchAll(json, \"Water\");\n//     console.log(obj);\n//   });\n\nfunction searchAll(json, text) {\n  if (!json) return null;\n\n  if (typeof json === \"string\" && json.includes(text)) {\n    return json;\n  }\n\n  if (typeof json === \"object\") {\n    for (const key in json) {\n      const found = searchAll(json[key], text);\n      if (found) return found;\n    }\n  }\n}\n\n\n//# sourceURL=webpack:///./script.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _components_header_header__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/header/header */ \"./components/header/header.js\");\n/* harmony import */ var _components_homePage_home__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./components/homePage/home */ \"./components/homePage/home.js\");\n\n\n\nconsole.log(\"start stef\");\nObject(_components_header_header__WEBPACK_IMPORTED_MODULE_0__[\"createHeader\"])();\nObject(_components_homePage_home__WEBPACK_IMPORTED_MODULE_1__[\"generateHomePage\"])();\n\n\n//# sourceURL=webpack:///./main.js?");
 
 /***/ })
 
