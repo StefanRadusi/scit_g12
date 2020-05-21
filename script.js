6e490cc27ac15cabf30d2d3a68c36446561421e1
mariancerlinca <mariancerlinca93@gmail.com>
2020-05-21 15:53:20 +0300

    HW

--- script.js
+++ script.js
@@ -1,54 +1,203 @@
 console.log("start");
 
-const DEFAULT_MOVE_INCREMENT = 2;
+const DEFAULT_MOVE_INCREMENT = 15;
 
 class Player {
   constructor() {
     this.domElement = document.getElementById("player");
-    // here we store the initial position of the player
+    this.obstacles = document.querySelectorAll(".obstacle");
+    this.gameContainer = document.getElementById("game");
+    this.alienShip = document.getElementById("transport");
+
+    this.stateChecker = 0;
     this.top = 0;
     this.left = 0;
 
-    // here we call the function that sets the behaviour of our player
+    // this.soundTrack();
+    this.theMovingObstacle();
+    this.createLivesContainer();
     this.setMovement();
   }
 
+  soundTrack() {
+    const soundTrack = document.createElement("audio");
+    soundTrack.style.display = "none";
+    soundTrack.src = "./scit_g12/The X Files theme.mp3";
+    soundTrack.autoplay = true;
+    soundTrack.loop = true;
+    document.body.appendChild(soundTrack);
+  }
+
   setMovement() {
-    // because we are interested in reacting to keyboard press on the window in general not on a specific DOM element, we will set an event listener on document
-    // we must use bind so that "this" will refer (in the "movePlayer" method) to "Player"  object otherwise "this" would refer to "document" object because there is the context on which we added the event listener
     document.addEventListener("keydown", this.movePlayer);
   }
 
-  movePlayer = event => {
-    // event is the default parameter that it is passed to any callback function of an "addEventListener"
-    // event is a complex object that contains information regarding the event that was trigger by browser
-    // in this case the event is of type "keydown" but we need to react only to some type "keydown" event
-    switch (event.key) {
-      case "ArrowDown":
-        this.top = this.top + DEFAULT_MOVE_INCREMENT;
+  movePlayer = (event, state) => {
+    const lives = document.getElementById("lives");
 
-        // we move the white div which is out player by changing it position
-        // the payer is initial positions at top = 0 and left = 0
-        // if we change this style attributes on the dom elements then we change the position in the window thus making the affect of moving a element
-        this.domElement.style.top = this.top + "px";
-        break;
-      case "ArrowUp":
-        this.top = this.top - DEFAULT_MOVE_INCREMENT;
-        this.domElement.style.top = this.top + "px";
-        break;
-      case "ArrowLeft":
-        this.left = this.left - DEFAULT_MOVE_INCREMENT;
-        this.domElement.style.left = this.left + "px";
-        break;
-      case "ArrowRight":
-        this.left = this.left + DEFAULT_MOVE_INCREMENT;
-        this.domElement.style.left = this.left + "px";
-        break;
+    this.state = this.stateChecker;
+    console.log(this.state);
 
-      default:
-        break;
+    this.handleObstacles();
+    this.handleWin(state);
+
+    if (this.state !== 1 && lives.innerHTML > 0) {
+      switch (event.key) {
+        case "ArrowDown":
+          if (this.top < 284) {
+            this.top = this.top + DEFAULT_MOVE_INCREMENT;
+            this.domElement.style.top = this.top + "px";
+          }
+          break;
+        case "ArrowUp":
+          if (this.top > 0) {
+            this.top = this.top - DEFAULT_MOVE_INCREMENT;
+            this.domElement.style.top = this.top + "px";
+          }
+          break;
+        case "ArrowLeft":
+          if (this.left > 0) {
+            this.left = this.left - DEFAULT_MOVE_INCREMENT;
+            this.domElement.style.left = this.left + "px";
+          }
+          break;
+        case "ArrowRight":
+          if (this.left < 484) {
+            this.left = this.left + DEFAULT_MOVE_INCREMENT;
+            this.domElement.style.left = this.left + "px";
+          }
+          break;
+
+        default:
+          break;
+      }
     }
   };
+
+  handleObstacles() {
+    const player = this.domElement.getBoundingClientRect();
+
+    this.obstacles.forEach((obstacle) => {
+      const theObstacles = obstacle.getBoundingClientRect();
+      if (
+        theObstacles.left <= player.right &&
+        theObstacles.right >= player.left &&
+        theObstacles.top <= player.bottom &&
+        theObstacles.bottom >= player.top
+      ) {
+        this.handleLostLives();
+      }
+    });
+  }
+  handleWin(state) {
+    if (this.top < 36 && this.left > 460) {
+      let lives = document.querySelectorAll(".title");
+      lives.forEach((element) => {
+        element.innerHTML = null;
+      });
+      this.stateChecker = 1;
+      this.gameContainer.innerHTML = "You win HUMAN !";
+      this.gameContainer.id = "win";
+      this.resetButton();
+      this.yodaImgLayer();
+      this.stateCheck(this.stateChecker);
+    }
+  }
+
+  yodaImgLayer() {
+    const reset = document.getElementById("reset");
+    const layer = document.createElement("img");
+    layer.id = "babyYoda";
+    layer.src = "./scit_g12/babyYoda.png";
+    this.gameContainer.appendChild(layer);
+  }
+
+  stateCheck(state) {
+    return state;
+  }
+
+  handleLostLives() {
+    const heart3 = document.getElementById("heart3");
+    const heart2 = document.getElementById("heart2");
+    const heart1 = document.getElementById("heart1");
+
+    const lives = document.getElementById("lives");
+    this.top = 0;
+    this.left = 0;
+    this.domElement.style.top = this.top + "px";
+    this.domElement.style.left = this.left + "px";
+    if (lives.innerHTML) {
+      lives.innerHTML = lives.innerHTML - 1;
+    }
+    if (lives.innerHTML === "2") {
+      heart3.classList.add("heartKiller");
+    } else if (lives.innerHTML === "1") {
+      heart2.classList.add("heartKiller");
+    } else if (lives.innerHTML === "0") {
+      heart1.classList.add("heartKiller");
+    }
+    if (lives.innerHTML === "0" && this.stateChecker !== 1) {
+      this.handleDefeat();
+    }
+  }
+
+  handleDefeat() {
+    this.gameContainer.innerHTML = "You lose !";
+    this.gameContainer.id = "lose";
+    this.resetButton();
+  }
+
+  resetButton() {
+    const reset = document.createElement("button");
+    this.gameContainer.appendChild(reset);
+    reset.id = "reset";
+    reset.innerHTML = "New Game";
+
+    reset.addEventListener("click", this.handleReset);
+  }
+
+  handleReset() {
+    location.reload(true);
+  }
+
+  createLivesContainer() {
+    let heartContainer = document.createElement("div");
+    heartContainer.id = "heartContainer";
+    document.body.appendChild(heartContainer);
+
+    let heart1 = document.createElement("img");
+    heart1.id = "heart1";
+    heart1.src = "/scit_g12/heart.png";
+    heartContainer.appendChild(heart1);
+
+    let heart2 = document.createElement("img");
+    heart2.id = "heart2";
+    heart2.src = "/scit_g12/heart.png";
+    heartContainer.appendChild(heart2);
+
+    let heart3 = document.createElement("img");
+    heart3.id = "heart3";
+    heart3.src = "/scit_g12/heart.png";
+    heartContainer.appendChild(heart3);
+  }
+
+  theMovingObstacle() {
+    let o7 = document.getElementById("o7");
+    let mover = 0;
+    let moverBot = 0;
+    setInterval(function () {
+      console.log(o7.offsetTop);
+
+      if (o7.offsetTop < 150) {
+        o7.style.top = o7.offsetTop + 10;
+      } else if (o7.offsetTop > 0) {
+        {
+          moverBot = o7.offsetTop - 150;
+          o7.style.top = moverBot;
+        }
+      }
+    }, 150);
+  }
 }
 
 new Player();
