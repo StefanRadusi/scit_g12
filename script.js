6e5fe5d48b62d5014fc68401ffebd10f1ce74f99
AlexandruNasalean <alexandru.nasalean@outlook.com>
2020-05-25 16:07:11 +0300

    up

--- script.js
+++ script.js
@@ -3,9 +3,21 @@ console.log("start");
 // example of reacting to button click
 const sendButton = document.getElementById("send");
 
+const InputField =document.getElementById("fieldName").addEventListener('keypress', onClickSendButton);
+
+function createNewField() {
+
+ 
+
+}
 // this the function that will be called when click event is triggered
 function onClickSendButton() {
-  console.log("click on", sendButton);
+  
+  const newDiv = document.createElement("div");
+  const newfieldName = document.createElement("p");
+  const newinputField = document.createElement("input");
+  newfieldName.appendChild(newinputField);
+  newDiv.appendChild(newfieldName);
 }
 
 // we attached a function to click event triggered by send button
