ac7302bce914dc9ad5be0e65555c4662225b3021
AlexandruNasalean <alexandru.nasalean@outlook.com>
2020-05-26 14:32:36 +0300

    up

--- script.js
+++ script.js
@@ -1,47 +1,53 @@
 console.log("start");
 
 // example of reacting to button click
-const sendButton = document.getElementById("send");
+// const sendButton = document.getElementById("send");
 
-const newFieldData = document.getElementById("fieldName")
+const newFieldData = document.getElementById("addFiled")
 newFieldData.addEventListener("click", onClickAddFields);
 
 function onClickAddFields() {
+  const mainDivContainer = document.getElementById("form-fields")
   const InputData = document.getElementById("fieldName").value;
   console.log(InputData)
   if(InputData === "" ){
     document.getElementById("fieldName").style.borderColor = "red";
   }else{
-    document.getElementById("fieldName").style.borderColor = "lightgray";
+    document.getElementById("fieldName").style.borderColor = "1px solid lightgray";
 
     const newDivContainer = document.createElement("div");
     newDivContainer.className = "field";
 
     const newParagraph = document.createElement("p");
     newParagraph.innerText = InputData;
+    // console.log(newParagraph)
 
     const newInputField = document.createElement("input")
+    // console.log(newInputField)
     newInputField.innerText = InputData;
+
+
 
     newDivContainer.appendChild(newParagraph);
     newDivContainer.appendChild(newInputField);
-    document.getElementById("form-fields").appendChild(newDivContainer)
+    mainDivContainer.appendChild(newDivContainer)
+    console.log(newDivContainer)
 
   }
   
 }
-function onClickSendButton() {
+// function onClickSendButton() {
 
-  var formContainer = document.getElementById("form-container")
-  var div = document.createElement("div");
+//   var formContainer = document.getElementById("form-container")
+//   var div = document.createElement("div");
 
-  var paragraph = document.createElement("p").addEventListener('keypress')
+//   var paragraph = document.createElement("p").addEventListener('keypress')
   
-  var text = document.createTextNode("Hello World");
-  div.appendChild(paragraph);
-  paragraph.appendChild(text)
-  document.body.appendChild(h);
-}
+//   var text = document.createTextNode("Hello World");
+//   div.appendChild(paragraph);
+//   paragraph.appendChild(text)
+//   document.body.appendChild(h);
+// }
 
-// we attached a function to click event triggered by send button
-sendButton.addEventListener("click", onClickAddFields);
+// // we attached a function to click event triggered by send button
+// sendButton.addEventListener("click", onClickAddFields);
