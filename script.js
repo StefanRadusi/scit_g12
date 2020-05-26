5b7d5980c2b25a5485a02a1c7030ba51577c6a7b
AlexandruNasalean <alexandru.nasalean@outlook.com>
2020-05-26 19:19:29 +0300

    up

--- script.js
+++ script.js
@@ -1,58 +1,41 @@
-// class constructor
-// this function is called when you instantiate a object with key word "new"
 
 function HangMan() {
-  // this is a attribute which all instantiated object with this class will have
-  // keep in mind even thou all the object will have this attribute, the value of the attribute will be different because of the "Random" implementation
   this.lettersDOM = [];
   this.mistakes = 0;
   this.okLetter =0;
 }
 
-HangMan.prototype.chooseWord = function() {
+HangMan.prototype.wordRandomizer = function() {
   const words = ["cars", "cat", "donkey", "star", "africa", "jaggermeister"];
   this.word = words[Math.floor(Math.random() * words.length)];
 }
 
-// "renderUnderScores" is a method off the HangMan class
-// a method is a functionality of an object, and is a function
 HangMan.prototype.renderUnderScores = function() {
-  // "this" key word represents the current object
-  // we need "this" so we can define general function which can apply on all different instances of the class
-  // the "for" loop is used to create paragraphs to render for every letter in the computer chosen word  
   for (let i = 0; i < this.words.length; i++) {
     const p = document.createElement("p");
     p.innerText = "_";
     p.classList.add("word");
     document.getElementById("letters").appendChild(p);
-
-    // we store the new created dom object in in the objects attribute for latter use 
     this.lettersDOM.push(p);
   }
 };
 
 
 HangMan.prototype.getInputFromUser = function() {
-  // we use this technic because "this" inside off all callback is NOT the current object; 
   const obj = this;
   document.getElementById("input").addEventListener("keydown", function(event) {
     if (event.key === "Enter") {
       const text = event.target.value;
-
       if (text.length === 1) {
         console.log(text);
-        // "obj" contains "this" and this is HangMan class, so we can call "checkForLetter" method
         obj.checkForLetter(text);
       }
-
       event.target.value = "";
     }
   });
 };
 
-// this method contains the logic for deciding if the user has chose a correct letter or not
 HangMan.prototype.checkForLetter = function(letter) {
-
   if (this.word.includes(letter)) {
     this.matchLetter(letter);
   } else {
